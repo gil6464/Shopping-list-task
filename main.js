e207cbe66146d08951c51b50db14ccbf98ac0322
gil6464 <77244365+gil6464@users.noreply.github.com>
2021-01-24 15:32:43 +0200

    create function "add button"
    create the function and put three var inside it

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 const shoppingList = document.querySelector("ul");
 const input = document.querySelector("input");
 const addButton = document.querySelector("button"); 
+
+  addButton.onclick = function() {
+      let theItem = input.value;
+      input.value = '';
+      
+     const listItem = document.createElement("li");
+     const spanText = document.createElement("span");
+     const deleteButton = document.createElement("button");
+
+  }
